21e6aada89317ba2bd5db8bfe0009730ab278830
PNA7 <Patryk.NAJDA.intern@3ds.com>
2025-01-14 16:48:34 +0100

    Local MVP changes to be continued today...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import * as XLSX from 'xlsx'
 import { useState } from 'react';
+import {useForm} from 'react-hook-form'
+import * as Calendar from 'react-calendar'
+import moment from 'moment'
 import './App.css';
 
 function App() {
     const [data, setData] = useState([]);
+    const [criteria, setCriteria] = useState({})
+
+    const {register, handleSubmit, watch} = useForm()
+    const selectedDegree = watch('selectedDegree')
+
+    const changeCriteria = (data) => {
+      console.log(data)
+      setCriteria(data)
+    }
   
     const handleFileChange = (e) => {
       const file = e.target.files[0];
@@ -30,8 +42,8 @@ function App() {
           const worksheet = workbook.Sheets[sheetName];
           const jsonData = XLSX.utils.sheet_to_json(worksheet);
   
-          setData(jsonData);
-          console.log(data)
+          setData(jsonData.filter((a, index) => jsonData[index] && index <= 178));
+          console.log(jsonData.filter((a, index) => jsonData[index] && index <= 178))
         } catch (error) {
           console.error('Błąd parsowania pliku:', error);
         }
@@ -48,10 +60,154 @@ function App() {
   return (
     <div className="App">
       <input type='file' onChange={handleFileChange}/>
+      <form onSubmit={handleSubmit(changeCriteria)}>
+        <label>Wybierz stopień studiów</label>
+        <select {...register('selectedDegree')}>
+          <option value="1">I stopnia</option>
+          <option value="2">II stopnia</option>
+        </select>
+        {(selectedDegree && selectedDegree == "2") && <div>
+            <label>Wybierz specjalizację</label>
+            <select {...register('selectedSpecialization')}>
+              <option value="cybsec">Cyberbezpieczeństwo</option>
+              <option value="ds">Data Science</option>
+            </select>
+            <br/>
+            <label>Wybierz semestr</label>
+            <select {...register('selectedTerm')}>
+              <option value="sem1">Pierwszy</option>
+              <option value="sem3">Trzeci</option>
+            </select>
+            <br/>
+            <label>Wybierz grupę</label>
+            <select {...register('selectedGroup')}>
+              <option value="CY1">CY1</option>
+              <option value="CY2">CY2</option>
+              <option value="CY3">CY3</option>
+              <option value="DS1">DS1</option>
+              <option value="DS2">DS2</option>
+            </select>
+          </div>
+        }
+        {(selectedDegree && selectedDegree == "1") && <div>
+            <label>Wybierz semestr</label>
+            <select {...register('selectedTerm')}>
+              <option value="sem1">Pierwszy</option>
+              <option value="sem3">Trzeci</option>
+              <option value="sem5">Piąty</option>
+              <option value="sem7">Siódmy</option>
+            </select>
+            <br/>
+            <label>Wybierz grupę</label>
+            <select {...register('selectedGroup')}>
+              <option value="11">11</option>
+              <option value="12">12</option>
+              <option value="13">13</option>
+              <option value="21">21</option>
+              <option value="22">22</option>
+              <option value="31">31</option>
+              <option value="32">32</option>
+              <option value="41">41</option>
+              <option value="42">42</option>
+            </select>
+          </div>
+        }
+        <input type="submit" value="Submit"/>
+      </form>
       {data ? data.map(item => {
-         return (<div>
-            {JSON.stringify(item)}
-          </div>)})
+         if(criteria && criteria['selectedDegree'] === '1' && criteria['selectedTerm'] === 'sem1' && criteria['selectedGroup'] === '11') {
+          return (<div>
+            {moment.unix(Number(JSON.stringify(item["__EMPTY"]))).format('YYYY-MM-DD')}
+            {JSON.stringify(item["__EMPTY_1"])}
+            {JSON.stringify(item["__EMPTY_3"])}
+          </div>)
+         } else if(criteria && criteria['selectedDegree'] === '1' && criteria['selectedTerm'] === 'sem1' && criteria['selectedGroup'] === '12') {
+          return (<div>
+            {JSON.stringify(item["__EMPTY_1"])}
+            {JSON.stringify(item["__EMPTY_5"])}
+          </div>)
+         } else if(criteria && criteria['selectedDegree'] === '1' && criteria['selectedTerm'] === 'sem1' && criteria['selectedGroup'] === '13') {
+          return (<div>
+            {JSON.stringify(item["__EMPTY_1"])}
+            {JSON.stringify(item["__EMPTY_7"])}
+          </div>)
+         } else if(criteria && criteria['selectedDegree'] === '1' && criteria['selectedTerm'] === 'sem3' && criteria['selectedGroup'] === '21') {
+          return (<div>
+            {JSON.stringify(item["__EMPTY_1"])}
+            {JSON.stringify(item["__EMPTY_8"])}
+          </div>)
+         } else if(criteria && criteria['selectedDegree'] === '1' && criteria['selectedTerm'] === 'sem3' && criteria['selectedGroup'] === '22') {
+          return (<div>
+            {JSON.stringify(item["__EMPTY_1"])}
+            {JSON.stringify(item["__EMPTY_10"])}
+          </div>)
+         } else if(criteria && criteria['selectedDegree'] === '1' && criteria['selectedTerm'] === 'sem5' && criteria['selectedGroup'] === '31') {
+          return (<div>
+            {JSON.stringify(item["__EMPTY_1"])}
+            {JSON.stringify(item["__EMPTY_12"])}
+          </div>)
+         } else if(criteria && criteria['selectedDegree'] === '1' && criteria['selectedTerm'] === 'sem5' && criteria['selectedGroup'] === '32') {
+          return (<div>
+            {JSON.stringify(item["__EMPTY_1"])}
+            {JSON.stringify(item["__EMPTY_14"])}
+          </div>)
+         } else if(criteria && criteria['selectedDegree'] === '1' && criteria['selectedTerm'] === 'sem7' && criteria['selectedGroup'] === '41') {
+          return (<div>
+            {JSON.stringify(item["__EMPTY_1"])}
+            {JSON.stringify(item["__EMPTY_16"])}
+          </div>)
+         } else if(criteria && criteria['selectedDegree'] === '1' && criteria['selectedTerm'] === 'sem7' && criteria['selectedGroup'] === '42') {
+          return (<div>
+            {JSON.stringify(item["__EMPTY_1"])}
+            {JSON.stringify(item["__EMPTY_18"])}
+          </div>)
+         } else if(criteria && criteria['selectedDegree'] === '2' && criteria['selectedTerm'] === 'sem1' && criteria['selectedSpecialization'] === 'ds' && criteria['selectedGroup'] === 'DS1') {
+          return (<div>
+            {JSON.stringify(item["__EMPTY_1"])}
+            {JSON.stringify(item["__EMPTY_22"])}
+          </div>)
+         } else if(criteria && criteria['selectedDegree'] === '2' && criteria['selectedTerm'] === 'sem1' && criteria['selectedSpecialization'] === 'ds' && criteria['selectedGroup'] === 'DS2') {
+            return (<div>
+              {JSON.stringify(item["__EMPTY_1"])}
+              {JSON.stringify(item["__EMPTY_23"])}
+            </div>)
+         } else if(criteria && criteria['selectedDegree'] === '2' && criteria['selectedTerm'] === 'sem1' && criteria['selectedSpecialization'] === 'cybsec' && criteria['selectedGroup'] === 'CY1') {
+          return (<div>
+            {JSON.stringify(item["__EMPTY_1"])}
+            {JSON.stringify(item["__EMPTY_24"])}
+          </div>)
+        } else if(criteria && criteria['selectedDegree'] === '2' && criteria['selectedTerm'] === 'sem1' && criteria['selectedSpecialization'] === 'cybsec' && criteria['selectedGroup'] === 'CY2') {
+          return (<div>
+            {JSON.stringify(item["__EMPTY_1"])}
+            {JSON.stringify(item["__EMPTY_25"])}
+          </div>)
+         } else if(criteria && criteria['selectedDegree'] === '2' && criteria['selectedTerm'] === 'sem1' && criteria['selectedSpecialization'] === 'cybsec' && criteria['selectedGroup'] === 'CY3') {
+            return (<div>
+              {JSON.stringify(item["__EMPTY_1"])}
+              {JSON.stringify(item["__EMPTY_26"])}
+            </div>)
+         } else if(criteria && criteria['selectedDegree'] === '2' && criteria['selectedTerm'] === 'sem3' && criteria['selectedSpecialization'] === 'cybsec' && criteria['selectedGroup'] === 'CY1') {
+          return (<div>
+            {JSON.stringify(item["__EMPTY_1"])}
+            {JSON.stringify(item["__EMPTY_27"])}
+          </div>)
+        } else if(criteria && criteria['selectedDegree'] === '2' && criteria['selectedTerm'] === 'sem3' && criteria['selectedSpecialization'] === 'cybsec' && criteria['selectedGroup'] === 'CY2') {
+            return (<div>
+              {JSON.stringify(item["__EMPTY_1"])}
+              {JSON.stringify(item["__EMPTY_28"])}
+            </div>)
+        } else if(criteria && criteria['selectedDegree'] === '2' && criteria['selectedTerm'] === 'sem3' && criteria['selectedSpecialization'] === 'cybsec' && criteria['selectedGroup'] === 'CY3') {
+          return (<div>
+            {JSON.stringify(item["__EMPTY_1"])}
+            {JSON.stringify(item["__EMPTY_29"])}
+          </div>)
+         } else if(criteria && criteria['selectedDegree'] === '2' && criteria['selectedTerm'] === 'sem3' && criteria['selectedSpecialization'] === 'ds' && criteria['selectedGroup'] === 'DS1') {
+          return (<div>
+            {JSON.stringify(item["__EMPTY_1"])}
+            {JSON.stringify(item["__EMPTY_30"])}
+          </div>)
+         }
+        })
          : null}
     </div>
   );
